db60275715c24e8d93052d809995a4ba777015ee
Christopher Bare <christopherbare@outlook.com>
2019-04-12 16:28:44 -0400

    fibonacci series example with recursion

---
+++ fibonacciRecursive.js
@@ -0,0 +1,14 @@
+function fibonacci(num) {
+  if (num <= 1) {
+      return 1;
+} else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+}
+
+let count = 1;
+for(let i = 0; i < 35; i++){
+    console.log(count + ": " + fibonacci(i));
+    count++;
+}
+
