645c016f70bc343205f4f1b84356164e5838fb7e
Christopher Bare <christopherbare@outlook.com>
2019-04-22 11:17:01 -0400

    Changed a thing or two

--- hello-world-react/src/components/Shelf.js
+++ hello-world-react/src/components/Shelf.js
@@ -1,27 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
 import Book from "./Book";
 import Nav from "./Nav";
 import "../css/Shelf.css";
 import books from "../assets/books.json";
 
 
-const Shelf = () => (
-  <div>
-    <Nav />
-    <div className="container">
-      <div className="div-board">
-        <div className="row">
-          {books.map(book => (
-            <Book source={book} />
-          ))}
+class Shelf extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="container">
+          <div className="div-board">
+            <button className="btn btn-success add-button btn-lg btn-block">
+              Add Books
+            </button>
+            <br />
+            <div className="row">
+              {books.map(book => (
+                <Book source={book} />
+              ))}
+            </div>
+          </div>
+          <br />
         </div>
       </div>
-      <br />
-      <button className="btn btn-success add-button btn-lg btn-block">
-        Add
-      </button>
-    </div>
-  </div>
-);
-
+    );
+  }
+}
 export default Shelf;
